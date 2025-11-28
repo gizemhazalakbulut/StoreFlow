02247e178e3577c39d75a2679f0127ce55d1e32f
gizemhazalakbulut <gizemhazalakbulut123@gmail.com>
2025-11-28 16:43:16 +0300

    Bölüm 6 - 47.ders

---
+++ StoreFlow/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreFlow.Context;
+using StoreFlow.Entities;
+
+namespace StoreFlow.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public CategoryController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            category.CategoryStatus = false; // Yeni kategori varsayılan olarak pasif
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("CategoryList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+    }
+}
--- StoreFlow/Controllers/LayoutController.cs
+++ StoreFlow/Controllers/LayoutController.cs
@@ -2,14 +2,9 @@
 
 namespace StoreFlow.Controllers
 {
-    public class DashboardController : Controller
+    public class LayoutController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Statistics()
         {
             return View();
         }
